5b9891f3e9d62e2148c3a7c8c08c741dbc663314
gregjose25131 <gregjose2513@gmail.com>
2024-02-21 19:44:33 -0400

    First Commit

---
+++ src/models/Persona.java
@@ -0,0 +1,54 @@
+
+package models;
+
+
+public class Persona {
+    
+    protected int cedula;
+    protected String nombre;
+    protected String apellido;
+    protected String correo;
+    
+    public Persona(int cedula, String nombre,String apellido, String correo){
+        super();
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+}
