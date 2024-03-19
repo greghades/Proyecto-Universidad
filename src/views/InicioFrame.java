5a8d828b1695b8429d9f834c42a7b13e31464acc
3rnestocs <3rnestocs@gmail.com>
2024-03-19 11:03:00 -0400

    Correcciones visuales en inicioframe.

--- src/views/InicioFrame.java
+++ src/views/InicioFrame.java
@@ -34,11 +34,11 @@ public class InicioFrame extends javax.swing.JFrame {
 
     private void agregarListeners(ActionListener accion) {
         inscripcion_button.addActionListener(accion);
-        reporte1_button.addActionListener(accion);
+        asignar_nota_button.addActionListener(accion);
     }
 
     public JButton getReporte1_button() {
-        return reporte1_button;
+        return asignar_nota_button;
     }
 
     public JButton getInscripcion_button() {
@@ -54,131 +54,152 @@ public class InicioFrame extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
-        java.awt.GridBagConstraints gridBagConstraints;
 
         title_panel = new javax.swing.JPanel();
         title_label2 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        title_label3 = new javax.swing.JLabel();
         inscripcion_button = new javax.swing.JButton();
-        title_label = new javax.swing.JLabel();
-        reporte1_button = new javax.swing.JButton();
-        reporte2_button = new javax.swing.JButton();
+        listado_estudiante_button = new javax.swing.JButton();
+        retirar_materia_button = new javax.swing.JButton();
+        listado_seccion_button = new javax.swing.JButton();
+        asignar_nota_button = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
 
         title_panel.setBackground(new java.awt.Color(58, 159, 220));
         title_panel.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(37, 92, 125), 2, true));
+        title_panel.setPreferredSize(new java.awt.Dimension(803, 94));
+        title_panel.setLayout(new java.awt.BorderLayout());
 
         title_label2.setFont(new java.awt.Font("Arial", 1, 36)); // NOI18N
         title_label2.setForeground(new java.awt.Color(255, 255, 255));
         title_label2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        title_label2.setText("Bienvenido");
-
-        javax.swing.GroupLayout title_panelLayout = new javax.swing.GroupLayout(title_panel);
-        title_panel.setLayout(title_panelLayout);
-        title_panelLayout.setHorizontalGroup(
-            title_panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(title_panelLayout.createSequentialGroup()
-                .addGap(189, 189, 189)
-                .addComponent(title_label2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGap(176, 176, 176))
-        );
-        title_panelLayout.setVerticalGroup(
-            title_panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, title_panelLayout.createSequentialGroup()
-                .addContainerGap(24, Short.MAX_VALUE)
-                .addComponent(title_label2)
-                .addContainerGap(24, Short.MAX_VALUE))
-        );
+        title_label2.setText("Sistema de gestion UCLA");
+        title_panel.add(title_label2, java.awt.BorderLayout.CENTER);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(58, 159, 220), 2, true));
-        jPanel1.setLayout(new java.awt.GridBagLayout());
+        jPanel1.setPreferredSize(new java.awt.Dimension(1214, 598));
+
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel2.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.CENTER, 24, 24));
+
+        title_label3.setBackground(new java.awt.Color(153, 0, 51));
+        title_label3.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
+        title_label3.setForeground(new java.awt.Color(51, 51, 51));
+        title_label3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        title_label3.setText("Opciones disponibles");
+        title_label3.setMaximumSize(new java.awt.Dimension(1000000, 28));
+        title_label3.setMinimumSize(new java.awt.Dimension(624, 28));
+        title_label3.setPreferredSize(new java.awt.Dimension(948, 28));
+        jPanel2.add(title_label3);
 
         inscripcion_button.setBackground(new java.awt.Color(58, 159, 220));
         inscripcion_button.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         inscripcion_button.setForeground(new java.awt.Color(255, 255, 255));
         inscripcion_button.setText("Inscribir estudiante");
         inscripcion_button.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(37, 92, 125), 2, true));
+        inscripcion_button.setPreferredSize(new java.awt.Dimension(300, 50));
         inscripcion_button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 inscripcion_buttonActionPerformed(evt);
             }
         });
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.ipadx = 48;
-        gridBagConstraints.ipady = 27;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(18, 54, 0, 0);
-        jPanel1.add(inscripcion_button, gridBagConstraints);
+        jPanel2.add(inscripcion_button);
 
-        title_label.setFont(new java.awt.Font("Segoe UI", 3, 36)); // NOI18N
-        title_label.setText("Proyecto Universidad");
-        jPanel1.add(title_label, new java.awt.GridBagConstraints());
-
-        reporte1_button.setBackground(new java.awt.Color(58, 159, 220));
-        reporte1_button.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
-        reporte1_button.setForeground(new java.awt.Color(255, 255, 255));
-        reporte1_button.setText("Reporte 1");
-        reporte1_button.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(37, 92, 125), 2, true));
-        reporte1_button.addActionListener(new java.awt.event.ActionListener() {
+        listado_estudiante_button.setBackground(new java.awt.Color(58, 159, 220));
+        listado_estudiante_button.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        listado_estudiante_button.setForeground(new java.awt.Color(255, 255, 255));
+        listado_estudiante_button.setText("Listados por estudiante");
+        listado_estudiante_button.setActionCommand("");
+        listado_estudiante_button.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(37, 92, 125), 2, true));
+        listado_estudiante_button.setPreferredSize(new java.awt.Dimension(300, 50));
+        listado_estudiante_button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                reporte1_buttonActionPerformed(evt);
+                listado_estudiante_buttonActionPerformed(evt);
             }
         });
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 2;
-        gridBagConstraints.ipadx = 158;
-        gridBagConstraints.ipady = 27;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(18, 54, 0, 0);
-        jPanel1.add(reporte1_button, gridBagConstraints);
+        jPanel2.add(listado_estudiante_button);
 
-        reporte2_button.setBackground(new java.awt.Color(58, 159, 220));
-        reporte2_button.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
-        reporte2_button.setForeground(new java.awt.Color(255, 255, 255));
-        reporte2_button.setText("Reporte 2");
-        reporte2_button.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(37, 92, 125), 2, true));
-        reporte2_button.addActionListener(new java.awt.event.ActionListener() {
+        retirar_materia_button.setBackground(new java.awt.Color(58, 159, 220));
+        retirar_materia_button.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        retirar_materia_button.setForeground(new java.awt.Color(255, 255, 255));
+        retirar_materia_button.setText("Retirar materia");
+        retirar_materia_button.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(37, 92, 125), 2, true));
+        retirar_materia_button.setPreferredSize(new java.awt.Dimension(300, 50));
+        retirar_materia_button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                reporte2_buttonActionPerformed(evt);
+                retirar_materia_buttonActionPerformed(evt);
             }
         });
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 3;
-        gridBagConstraints.ipadx = 158;
-        gridBagConstraints.ipady = 27;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(18, 54, 6, 0);
-        jPanel1.add(reporte2_button, gridBagConstraints);
+        jPanel2.add(retirar_materia_button);
+
+        listado_seccion_button.setBackground(new java.awt.Color(58, 159, 220));
+        listado_seccion_button.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        listado_seccion_button.setForeground(new java.awt.Color(255, 255, 255));
+        listado_seccion_button.setText("Listados por seccion");
+        listado_seccion_button.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(37, 92, 125), 2, true));
+        listado_seccion_button.setPreferredSize(new java.awt.Dimension(300, 50));
+        listado_seccion_button.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                listado_seccion_buttonActionPerformed(evt);
+            }
+        });
+        jPanel2.add(listado_seccion_button);
+
+        asignar_nota_button.setBackground(new java.awt.Color(58, 159, 220));
+        asignar_nota_button.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        asignar_nota_button.setForeground(new java.awt.Color(255, 255, 255));
+        asignar_nota_button.setText("Asignar nota estudiante");
+        asignar_nota_button.setToolTipText("");
+        asignar_nota_button.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(37, 92, 125), 2, true));
+        asignar_nota_button.setPreferredSize(new java.awt.Dimension(300, 50));
+        asignar_nota_button.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                asignar_nota_buttonActionPerformed(evt);
+            }
+        });
+        jPanel2.add(asignar_nota_button);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 986, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(220, 220, 220)
+                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, 538, Short.MAX_VALUE)
+                .addGap(16, 16, 16))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(8, 8, 8)
-                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 873, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(8, 8, 8)
-                        .addComponent(title_panel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                .addGap(8, 8, 8))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(16, 16, 16)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(title_panel, javax.swing.GroupLayout.DEFAULT_SIZE, 1002, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 1002, Short.MAX_VALUE))
+                .addGap(16, 16, 16))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(8, 8, 8)
+                .addGap(16, 16, 16)
                 .addComponent(title_panel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(8, 8, 8)
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 521, Short.MAX_VALUE)
-                .addGap(8, 8, 8))
+                .addGap(16, 16, 16)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 778, Short.MAX_VALUE)
+                .addGap(16, 16, 16))
         );
 
         pack();
@@ -188,22 +209,33 @@ public class InicioFrame extends javax.swing.JFrame {
 
     }//GEN-LAST:event_inscripcion_buttonActionPerformed
 
-    private void reporte1_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_reporte1_buttonActionPerformed
+    private void asignar_nota_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_asignar_nota_buttonActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_reporte1_buttonActionPerformed
+    }//GEN-LAST:event_asignar_nota_buttonActionPerformed
 
-    private void reporte2_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_reporte2_buttonActionPerformed
+    private void listado_seccion_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_listado_seccion_buttonActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_reporte2_buttonActionPerformed
+    }//GEN-LAST:event_listado_seccion_buttonActionPerformed
+
+    private void retirar_materia_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_retirar_materia_buttonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_retirar_materia_buttonActionPerformed
+
+    private void listado_estudiante_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_listado_estudiante_buttonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_listado_estudiante_buttonActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton asignar_nota_button;
     private javax.swing.JButton inscripcion_button;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JButton reporte1_button;
-    private javax.swing.JButton reporte2_button;
-    private javax.swing.JLabel title_label;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JButton listado_estudiante_button;
+    private javax.swing.JButton listado_seccion_button;
+    private javax.swing.JButton retirar_materia_button;
     private javax.swing.JLabel title_label2;
+    private javax.swing.JLabel title_label3;
     private javax.swing.JPanel title_panel;
     // End of variables declaration//GEN-END:variables
 }
